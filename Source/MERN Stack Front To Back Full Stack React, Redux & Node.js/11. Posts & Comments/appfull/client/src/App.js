2e24bbe67c8846721b456d2a4415c498b38c3756
phuong <tranduyphuong20100@gmail.com>
2020-05-05 17:57:55 +0700

    add source

---
+++ Source/MERN Stack Front To Back Full Stack React, Redux & Node.js/11. Posts & Comments/appfull/client/src/App.js
@@ -0,0 +1,75 @@
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import './App.css';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+import setAuthToken from '../src/utils/setAuthToken';
+import { loadUser } from './actions/auth';
+import Dashboard from './components/dashboard/Dashboard';
+import ProfileForm from './components/profile-forms/ProfileForm';
+import PrivateRoute from './components/routing/PrivateRoute';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import Post from './components/post/Post';
+import Posts from './components/posts/Posts';
+
+const App = () => {
+  useEffect(() => {
+    setAuthToken(localStorage.token);
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <div className='container'>
+            <Alert></Alert>
+            <Switch>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={ProfileForm}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={ProfileForm}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
+              />
+              <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
